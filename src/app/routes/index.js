694ba36b8644e0ee160470194a9277e3195062ba
Yash Lodaya <iyashlodaya@gmail.com>
2025-12-16 17:03:53 +0530

    feat: Add and register project routes.

--- src/app/routes/index.js
+++ src/app/routes/index.js
@@ -2,13 +2,15 @@ const fp = require('fastify-plugin');
 const waitlistRoutes = require('./waitlist.routes');
 const authRoutes = require('./auth.routes');
 const clientRoutes = require('./client.routes');
+const projectRoutes = require('./project.routes');
 
 async function routes(fastify) {
   fastify.get('/health', async () => ({ status: 'ok' }));
 
   await fastify.register(waitlistRoutes, { prefix: '/api/v1' })
   await fastify.register(authRoutes, { prefix: '/api/v1' });
-  await fastify.register(clientRoutes, { prefix: '/api/v1' });
+  await fastify.register(clientRoutes, { prefix: '/api/v1' })
+  await fastify.register(projectRoutes, { prefix: '/api/v1' })
 }
 
 module.exports = fp(routes);
