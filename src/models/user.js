7acbd495fb6942e2bdccee96aa4904e8dd4b7c30
Yash Lodaya <iyashlodaya@gmail.com>
2026-02-17 12:41:36 +0530

    feat: Implement user branding management and integrate it into public portal freelancer display.

--- src/models/user.js
+++ src/models/user.js
@@ -3,7 +3,7 @@ const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
     class User extends Model {
         static associate(models) {
-            // define association here
+            User.hasOne(models.PortalBranding, { foreignKey: 'user_id', as: 'branding' });
         }
     }
     User.init({
